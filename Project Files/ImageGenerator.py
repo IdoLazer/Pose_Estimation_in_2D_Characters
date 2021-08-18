bfe4892cde88d6149ba0f3e64174d55bfe7b6fd5
IdoLazer <idolazer8@gmail.com>
2021-08-18 19:16:59 +0300

    New characters and serialization method

--- Project Files/ImageGenerator.py
+++ Project Files/ImageGenerator.py
@@ -50,12 +50,10 @@ class Vector2D:
 IMAGE_SIZE = 128
 CHARACTER_PATH = PATH + 'Character Layers\\'
 CHARACTER_DICT = {
-    # 'Lower Torso': {'name': 'Lower Torso', 'path': CHARACTER_PATH + 'Character.png', 'displacement': Vector2D(0, 8),
-    #                 'children': ['Chest', 'Left Shoulder', 'Upper Right Leg'], 'parent': 'None'},
-    'Lower Torso': {'name': 'Lower Torso', 'path': CHARACTER_PATH + 'Lower Torso.png', 'displacement': Vector2D(0, 8),
-                    'children': ['Chest', 'Left Shoulder', 'Upper Right Leg'], 'parent': 'None'},
+    'Root': {'name': 'Root', 'path': CHARACTER_PATH + 'Lower Torso.png', 'displacement': Vector2D(0, 8),
+             'children': ['Chest', 'Left Shoulder', 'Upper Right Leg'], 'parent': 'None'},
     'Chest': {'name': 'Chest', 'path': CHARACTER_PATH + 'Chest.png', 'displacement': Vector2D(0, 7),
-              'children': ['Head', 'Left Shoulder', 'Right Shoulder'], 'parent': 'Lower Torso'},
+              'children': ['Head', 'Left Shoulder', 'Right Shoulder'], 'parent': 'Root'},
     'Head': {'name': 'Head', 'path': CHARACTER_PATH + 'Head.png', 'displacement': Vector2D(0, 20),
              'children': [], 'parent': 'Chest'},
     'Left Shoulder': {'name': 'Left Shoulder', 'path': CHARACTER_PATH + 'Left Shoulder.png',
@@ -70,19 +68,19 @@ CHARACTER_DICT = {
                   'children': [], 'parent': 'Right Shoulder'},
     'Upper Left Leg': {'name': 'Upper Left Leg', 'path': CHARACTER_PATH + 'Upper Left Leg.png',
                        'displacement': Vector2D(-5, -8),
-                       'children': ['Lower Left Leg'], 'parent': 'Lower Torso'},
+                       'children': ['Lower Left Leg'], 'parent': 'Root'},
     'Lower Left Leg': {'name': 'Lower Left Leg', 'path': CHARACTER_PATH + 'Lower Left Leg.png',
                        'displacement': Vector2D(0, -28),
                        'children': [''], 'parent': 'Upper Left Leg'},
     'Upper Right Leg': {'name': 'Upper Right Leg', 'path': CHARACTER_PATH + 'Upper Right Leg.png',
                         'displacement': Vector2D(5, -8),
-                        'children': ['Lower Right Leg'], 'parent': 'Lower Torso'},
+                        'children': ['Lower Right Leg'], 'parent': 'Root'},
     'Lower Right Leg': {'name': 'Lower Right Leg', 'path': CHARACTER_PATH + 'Lower Right Leg.png',
                         'displacement': Vector2D(1, -26),
                         'children': [''], 'parent': 'Upper Right Leg'},
 
 }
-DRAWING_ORDER = ['Lower Torso', 'Upper Left Leg', 'Lower Left Leg', 'Upper Right Leg', 'Lower Right Leg', 'Chest',
+DRAWING_ORDER = ['Root', 'Upper Left Leg', 'Lower Left Leg', 'Upper Right Leg', 'Lower Right Leg', 'Chest',
                  'Head', 'Left Shoulder', 'Left Arm', 'Right Shoulder', 'Right Arm']
 PARENTS = [None, 0, 1, 0, 3, 0, 5, 5, 7, 5, 9]
 
@@ -90,6 +88,54 @@ images = dict()
 
 # sample_params = [5, 14, -29, -16, 22, -10, -22, 14, -21, -26, -21]
 sample_params = [5, 14, -29, -16, 22, -10, -22, 14, -21, -26, -21]
+
+
+class Character:
+    image_size: int
+    path: str
+    hierarchy_dict: dict
+    drawing_order: list
+    parents: list
+    sample_params: list
+
+    def __init__(self):
+        self.image_size = 128
+        self.path = PATH + 'Character Layers\\Default Character\\'
+        self.hierarchy_dict = {
+            'Root': {'name': 'Root', 'path': self.path + 'Lower Torso.png',
+                     'displacement': (0, 8),
+                     'children': ['Chest', 'Left Shoulder', 'Upper Right Leg'], 'parent': 'None'},
+            'Chest': {'name': 'Chest', 'path': self.path + 'Chest.png', 'displacement': (0, 7),
+                      'children': ['Head', 'Left Shoulder', 'Right Shoulder'], 'parent': 'Root'},
+            'Head': {'name': 'Head', 'path': self.path + 'Head.png', 'displacement': (0, 20),
+                     'children': [], 'parent': 'Chest'},
+            'Left Shoulder': {'name': 'Left Shoulder', 'path': self.path + 'Left Shoulder.png',
+                              'displacement': (-10, 13),
+                              'children': ['Left Arm'], 'parent': 'Chest'},
+            'Left Arm': {'name': 'Left Arm', 'path': self.path + 'Left Arm.png', 'displacement': (-6, -15),
+                         'children': [], 'parent': 'Left Shoulder'},
+            'Right Shoulder': {'name': 'Right Shoulder', 'path': self.path + 'Right Shoulder.png',
+                               'displacement': (11, 13),
+                               'children': ['Right Arm'], 'parent': 'Chest'},
+            'Right Arm': {'name': 'Right Arm', 'path': self.path + 'Right Arm.png', 'displacement': (4, -16),
+                          'children': [], 'parent': 'Right Shoulder'},
+            'Upper Left Leg': {'name': 'Upper Left Leg', 'path': self.path + 'Upper Left Leg.png',
+                               'displacement': (-5, -8),
+                               'children': ['Lower Left Leg'], 'parent': 'Root'},
+            'Lower Left Leg': {'name': 'Lower Left Leg', 'path': self.path + 'Lower Left Leg.png',
+                               'displacement': (0, -28),
+                               'children': [''], 'parent': 'Upper Left Leg'},
+            'Upper Right Leg': {'name': 'Upper Right Leg', 'path': self.path + 'Upper Right Leg.png',
+                                'displacement': (5, -8),
+                                'children': ['Lower Right Leg'], 'parent': 'Root'},
+            'Lower Right Leg': {'name': 'Lower Right Leg', 'path': self.path + 'Lower Right Leg.png',
+                                'displacement': (1, -26),
+                                'children': [''], 'parent': 'Upper Right Leg'},
+        }
+        self.drawing_order = ['Root', 'Upper Left Leg', 'Lower Left Leg', 'Upper Right Leg', 'Lower Right Leg', 'Chest',
+                              'Head', 'Left Shoulder', 'Left Arm', 'Right Shoulder', 'Right Arm']
+        self.parents = [None, 0, 1, 0, 3, 0, 5, 5, 7, 5, 9]
+        self.sample_params = [0] * 11
 
 
 class BodyPart:
@@ -165,11 +211,11 @@ def create_affine_transform(angle, center, displacement, name, print_dict=False)
         [0, 0, 1]])
 
 
-def traverse_tree(cur, layers, draw_skeleton, skeleton_draw, transform=True, print_dict=False):
-    image_center = Vector2D(IMAGE_SIZE / 2, IMAGE_SIZE / 2)
+def traverse_tree(cur, size, layers, draw_skeleton, skeleton_draw, transform=True, print_dict=False):
+    image_center = Vector2D(size / 2, size / 2)
     im = cur.im
     if transform:
-        im = im.transform((IMAGE_SIZE, IMAGE_SIZE),
+        im = im.transform((size, size),
                           Image.AFFINE,
                           data=create_affine_transform(cur.rotation, image_center, cur.position, cur.name, print_dict).
                           flatten()[:6],
@@ -178,11 +224,11 @@ def traverse_tree(cur, layers, draw_skeleton, skeleton_draw, transform=True, pri
         if draw_skeleton:
             line = translate_points([cur.position, child.position], image_center)
             skeleton_draw.ellipse(
-                xy=(line[1].x - 1, IMAGE_SIZE - (line[1].y + 1), line[1].x + 1, IMAGE_SIZE - (line[1].y - 1),),
+                xy=(line[1].x - 1, size - (line[1].y + 1), line[1].x + 1, size - (line[1].y - 1),),
                 fill="red")
-            skeleton_draw.line([(line[0].x, IMAGE_SIZE - line[0].y),
-                                (line[1].x, IMAGE_SIZE - line[1].y)], fill="yellow")
-        traverse_tree(child, layers, draw_skeleton, skeleton_draw, transform, print_dict)
+            skeleton_draw.line([(line[0].x, size - line[0].y),
+                                (line[1].x, size - line[1].y)], fill="yellow")
+        traverse_tree(child, size, layers, draw_skeleton, skeleton_draw, transform, print_dict)
     if not cur.children and draw_skeleton:
         if cur.name == 'Head':
             line = translate_points([cur.position, rotate(cur.position, cur.position + Vector2D(0, 15), -cur.rotation)],
@@ -190,22 +236,23 @@ def traverse_tree(cur, layers, draw_skeleton, skeleton_draw, transform=True, pri
         else:
             line = translate_points(
                 [cur.position, rotate(cur.position, cur.position + Vector2D(0, -15), -cur.rotation)], image_center)
-        skeleton_draw.line([(line[0].x, IMAGE_SIZE - line[0].y),
-                            (line[1].x, IMAGE_SIZE - line[1].y)], fill="yellow")
+        skeleton_draw.line([(line[0].x, size - line[0].y),
+                            (line[1].x, size - line[1].y)], fill="yellow")
     layers[cur.name] = im
 
 
-def generate_layers(origin, draw_skeleton=False, as_tensor=False, transform=True, print_dict=False):
+def generate_layers(character, angles, draw_skeleton=False, as_tensor=False, transform=True, print_dict=False):
+    origin = create_body_hierarchy(angles, character)
     layers = {}
-    skeleton = Image.new('RGBA', (IMAGE_SIZE, IMAGE_SIZE))
+    skeleton = Image.new('RGBA', (character['image_size'], character['image_size']))
     skeleton_draw = ImageDraw.Draw(skeleton)
-    traverse_tree(origin, layers, draw_skeleton, skeleton_draw, transform, print_dict)
+    traverse_tree(origin, character['image_size'], layers, draw_skeleton, skeleton_draw, transform, print_dict)
     if not as_tensor:
         layers['Skeleton'] = skeleton
         return layers
     layers_list = []
-    for part in DRAWING_ORDER:
-        im = Image.new("RGBA", (IMAGE_SIZE, IMAGE_SIZE))
+    for part in character['drawing_order']:
+        im = Image.new("RGBA", (character['im_size'], character['im_size']))
         alpha = ImageOps.invert(layers[part].split()[-1])
         layer = Image.composite(im, layers[part], alpha)
         # layer = layer.convert("RGB")
@@ -227,11 +274,12 @@ def generate_layers(origin, draw_skeleton=False, as_tensor=False, transform=True
 #     return im
 
 
-def create_image(origin, draw_skeleton=False, omit_layers=False, print_dict=False):
-    drawing_order = DRAWING_ORDER + ['Skeleton']
-    layers = generate_layers(origin, draw_skeleton, print_dict=print_dict)
+def create_image(character, angles, draw_skeleton=False, omit_layers=False, print_dict=False, as_image=False):
+    drawing_order = DRAWING_ORDER + ['Skeleton'] if character is None else character['drawing_order'] + ['Skeleton']
+    layers = generate_layers(character, angles, draw_skeleton, print_dict=print_dict)
     # new_layers = []
-    im = Image.new("RGBA", (IMAGE_SIZE, IMAGE_SIZE))
+    im_size = IMAGE_SIZE if character is None else character['image_size']
+    im = Image.new("RGBA", (im_size, im_size))
     for part in drawing_order:
         if omit_layers and part != 'Right Arm':
             continue
@@ -240,22 +288,23 @@ def create_image(origin, draw_skeleton=False, omit_layers=False, print_dict=Fals
     # return np.array(new_layers).sum(axis=0).astype('uint8')
     # im = im.convert("RGB")
     # im.show()
-    return np.array(im).astype('uint8')
+    return im.convert("RGB") if as_image else np.array(im).astype('uint8')
 
 
-def create_body_hierarchy(parameters):
+def create_body_hierarchy(parameters, character=None):
     # print("sample_params = " + str(parameters))
-    for i in range(len(parameters)):
-        parameters[i] += sample_params[i]
+    if character is not None:
+        for i in range(len(parameters)):
+            parameters[i] += character['sample_params'][i]
     # parameters = sample_params  # TODO: Always comment out when starting
-    cur = CHARACTER_DICT['Lower Torso']
     parts_dict = dict()
     parts_dict['None'] = None
-    for i, part in enumerate(DRAWING_ORDER):
-        cur = CHARACTER_DICT[part]
+    for i, part in enumerate(DRAWING_ORDER if character is None else character['drawing_order']):
+        cur = CHARACTER_DICT[part] if character is None else character['hierarchy_dict'][part]
         parts_dict[part] = BodyPart(parts_dict[cur['parent']], cur['name'], cur['path'], None,
-                                    cur['displacement'], parameters[i], None)
-    return parts_dict['Lower Torso']
+                                    Vector2D(float(cur['displacement'][0]), float(cur['displacement'][1])),
+                                    parameters[i], None)
+    return parts_dict['Root']
 
 
 def load_data(batch_size=4):
@@ -268,8 +317,8 @@ def load_data(batch_size=4):
     i = 1
     for index in tqdm(range(len(labels))):
         angles = labels[index]
-        origin = create_body_hierarchy(angles)
-        im = create_image(origin, draw_skeleton=False, print_dict=False)#, omit_layers=(index // batch_size) % 2 == 1)
+        im = create_image(None, angles, draw_skeleton=False,
+                          print_dict=False)  # , omit_layers=(index // batch_size) % 2 == 1)
         im = (im - 127.5) / 127.5
         im_batch.append(im)
         label_batch.append(angles)
@@ -285,9 +334,13 @@ def load_data(batch_size=4):
     return data[:cutoff], data[cutoff:]
 
 
+import json
+
 if __name__ == "__main__":
-    angles = np.random.randint(-10, 10, 11)
-    origin = create_body_hierarchy(angles)
-    im = create_image(origin, draw_skeleton=False)
-    plt.imshow(im)
-    plt.show()
+    # char = Character()
+    # json.dump(char, open(char.path + 'Config', 'w'), default=lambda o: o.__dict__,
+    #         sort_keys=True, indent=4)
+    char = json.load(open(PATH + 'Character Layers\\Cartman\\Config.txt', 'r'))
+    angles = [0] * 11
+    im = create_image(char, angles, draw_skeleton=False, print_dict=False, as_image=True)
+    im.show()
